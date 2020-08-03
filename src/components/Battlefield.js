84c22866ce8700ecbf984cef8d7c076b057a73a6
Rapha-Y <raph.yuki@gmail.com>
2020-08-03 10:37:38 -0300

    added the main navigation bar

---
+++ src/components/Battlefield.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Battlefield() {
+    return (
+        <h1>Battlefield</h1>
+    );
+}
+
+export default Battlefield;
---
+++ src/components/BuyNow.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BuyNow() {
+    return (
+        <h1>BuyNow</h1>
+    );
+}
+
+export default BuyNow;
