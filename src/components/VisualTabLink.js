4bafb1ce2e6b4060a1ba45ce73d26545a6985c9a
Rapha-Y <raph.yuki@gmail.com>
2020-08-04 16:55:54 -0300

    fixed dynamic images problem

---
+++ src/components/VisualTabLink.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function VisualTabLink(props) {
+    return (
+        <Container className="bg-secondary my-3">
+            <Row>
+                <Col 
+                    md={5} 
+                    className="d-flex align-items-center justify-content-center border border-light"
+                >
+                    <Container className="m-5 text-center">
+                        <h1>{props.text}</h1>   
+                    </Container>
+                </Col>
+                <Col md={7} style={{padding: 0}}>
+                    <Image 
+                        src={props.img}
+                        className="max-width"
+                        fluid
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default VisualTabLink;
--- src/components/VisualTabLinks.js
+++ src/components/VisualTabLinks.js
@@ -1,70 +1,16 @@
 import React from 'react';
-import { Image, Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function VisualTabLinks() {
-    /*
-        The code repetition is due to an issue regarding passing Image src as a prop, as it doesn't work.
-        Issues must be fixed asap
-    */
-    
+import Repayment from '../resources/repayment.png';
+import VeteranKnight from '../resources/veteranknight.png';
+import Brands from '../resources/brands.png';
+import VisualTabLink from './VisualTabLink';
+
+function VisualTabLinks() {    
     return (
         <div>
-            <Container className="bg-secondary my-3">
-                <Row>
-                    <Col 
-                        md={5} 
-                        className="d-flex align-items-center justify-content-center border border-light"
-                    >
-                        <Container className="m-5 text-center">
-                            <h1>Meet the Characters</h1>   
-                        </Container>
-                    </Col>
-                    <Col md={7} style={{padding: 0}}>
-                        <Image 
-                            src={require("../resources/repayment.png")}
-                            className="max-width"
-                            fluid
-                        />
-                    </Col>
-                </Row>
-            </Container>
-            <Container className="bg-light my-3">
-                <Row>
-                    <Col 
-                        md={5} 
-                        className="d-flex align-items-center justify-content-center border border-secondary"
-                    >
-                        <Container className="m-5 text-center">
-                            <h1>Join the Battlefield</h1>
-                        </Container>
-                    </Col>
-                    <Col md={7} style={{padding: 0}}>
-                        <Image 
-                            src={require("../resources/veteranknight.png")}
-                            fluid
-                        />
-                    </Col>
-                </Row>
-            </Container>
-            <Container className="bg-secondary my-3">
-                <Row>
-                    <Col 
-                        md={5} 
-                        className="d-flex align-items-center justify-content-center border border-light"
-                    >
-                        <Container className="m-5 text-center">
-                            <h1>Start the Adventure</h1>
-                        </Container>
-                    </Col>
-                    <Col md={7} style={{padding: 0}}>
-                        <Image 
-                            src={require("../resources/brands.png")}
-                            fluid
-                        />
-                    </Col>
-                </Row>
-            </Container>
+            <VisualTabLink text="Meet the Characters" img={Repayment}/>
+            <VisualTabLink text="Join the Battlefield" img={VeteranKnight}/>
+            <VisualTabLink text="Start the Adventure" img={Brands}/>
         </div>
     );
 }
