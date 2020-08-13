16ec3d50a3e7c2f34015329f91fd43bf14855d2c
Rapha-Y <raph.yuki@gmail.com>
2020-08-13 18:28:49 -0300

    added character data

---
+++ src/resources/CharacterInfo.js
@@ -0,0 +1,437 @@
+import Alm from './portraits/alm.png';
+import Atlas from './portraits/atlas.png';
+import Berkut from './portraits/berkut.png';
+import Boey from './portraits/boey.png';
+import Catria from './portraits/catria.png';
+import Celica from './portraits/celica.png';
+import Clair from './portraits/clair.png';
+import Clive from './portraits/clive.png';
+import Conrad from './portraits/conrad.png';
+import Deen from './portraits/deen.png';
+import Delthea from './portraits/delthea.png';
+import Desaix from './portraits/desaix.png';
+import Duma from './portraits/duma.png';
+import Emma from './portraits/emma.png';
+import Est from './portraits/est.png';
+import Faye from './portraits/faye.png';
+import Fernand from './portraits/fernand.png';
+import Forsyth from './portraits/forsyth.png';
+import Genny from './portraits/genny.png';
+import Gray from './portraits/gray.png';
+import Grieth from './portraits/grieth.png';
+import Jedah from './portraits/jedah.png';
+import Jesse from './portraits/jesse.png';
+import Kamui from './portraits/kamui.png';
+import Kliff from './portraits/kliff.png';
+import Leon from './portraits/leon.png';
+import Lukas from './portraits/lukas.png';
+import Luthier from './portraits/luthier.png';
+import Mae from './portraits/mae.png';
+import Mathilda from './portraits/mathilda.png';
+import Mila from './portraits/mila.png';
+import Mycen from './portraits/mycen.png';
+import Nomah from './portraits/nomah.png';
+import Nuibaba from './portraits/nuibaba.png';
+import Palla from './portraits/palla.png';
+import Python from './portraits/python.png';
+import Randal from './portraits/randal.png';
+import Rinea from './portraits/rinea.png';
+import Rudolf from './portraits/rudolf.png';
+import Shade from './portraits/shade.png';
+import Silque from './portraits/silque.png';
+import Slayde from './portraits/slayde.png';
+import Sonya from './portraits/sonya.png';
+import Tatiana from './portraits/tatiana.png';
+import Tobin from './portraits/tobin.png';
+import Valbar from './portraits/valbar.png';
+import Yuzu from './portraits/yuzu.png';
+import Zeke from './portraits/zeke.png';
+
+const CharacterInfo = [
+    {
+        id: 1,
+        name: "Alm",
+        img: Alm,
+        class: "Fighter",
+        age: "17",
+        description: "One of the protagonists. He grew up in the village of Ram located at the borders of the Kingdom of Zofia. Trained by his grandfather Mycen, he has an exceptional sword arm. He joined the Deliverance to fight against Rigel to test his strength and because of his strong sense of justice."
+    },
+    {
+        id: 2,
+        name: "Atlas",
+        img: Atlas,
+        class: "Villager",
+        age: "20",
+        description: ""
+    },
+    {
+        id: 3,
+        name: "Berkut",
+        img: Berkut,
+        class: "Paladin",
+        age: "17",
+        description: "Born into the Rigelian royal family; the Emperor’s nephew. A highly ambitious and prideful man. In a bid to become the next emperor, he directed the invasion of Zofia to prove his military prowess."
+    },
+    {
+        id: 4,
+        name: "Boey",
+        img: Boey,
+        class: "Mage",
+        age: "16",
+        description: "A mage who works at the Novis Priory. He serves Celica together with his childhood friend Mae. He accompanies Celica on her journey towards the Temple of Mila."
+    },
+    {
+        id: 5,
+        name: "Catria",
+        img: Catria,
+        class: "Pegasus Knight",
+        age: "20",
+        description: "The middle sister of the three Whitewings of Archanea. Often acts as the buffer between her older and younger sisters and rarely pushes her own thoughts on the matter. Has a cool and serious personality."
+    },
+    {
+        id: 6,
+        name: "Celica",
+        img: Celica,
+        class: "Priestess",
+        age: "17",
+        description: "One of the protagonists. She spent her childhood days with Alm but left the village after a certain incident, serving as a priestess at Novis. After having an ominous dream about Alm and to determine the cause of many unusual happenings in Zofia, she leaves on a journey to the Temple of Mila."
+    },
+    {
+        id: 7,
+        name: "Clair",
+        img: Clair,
+        class: "Pegasus Knight",
+        age: "18",
+        description: "Clive’s younger sister; also a member of the Deliverance. Her overbearingness is her one big flaw, not that she pays it any mind. A cheery and pretty girl who loves to interact with any and everyone."
+    },
+    {
+        id: 8,
+        name: "Clive",
+        img: Clive,
+        class: "Cavalier",
+        age: "27",
+        description: "Leader of the Deliverance, a young man hailing from the upper nobility. Once a member of the Knights of Zofia, but in response to Desaix’s rebellion he created the Deliverance."
+    },
+    {
+        id: 9,
+        name: "Deen",
+        img: Deen,
+        class: "Myrmidon",
+        age: "30",
+        description: ""
+    },
+    {
+        id: 10,
+        name: "Delthea",
+        img: Delthea,
+        class: "Mage",
+        age: "13",
+        description: "Luthier’s little sister. Possesses a talent for magic that surpasses even her brother, but she herself is not interested in pursuing the Mage’s path. Bright and cheerful, but a bit of a selfish girl. Currently in the midst of a rebellious period against Luthier.",
+    },
+    {
+        id: 11,
+        name: "Desaix",
+        img: Desaix,
+        class: "Baron",
+        age: "43",
+        description: ""
+    },
+    {
+        id: 12,
+        name: "Duma",
+        img: Duma,
+        class: "???",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 13,
+        name: "Emma",
+        img: Emma,
+        class: "Falcon Knight",
+        age: "13",
+        description: ""
+    },
+    {
+        id: 14,
+        name: "Est",
+        img: Est,
+        class: "Pegasus Knight",
+        age: "16",
+        description: "The youngest of the three Whitewing sisters from Archanea. As the youngest child, she’s pure and innocent and often gets told off by her sisters. Cheerful and chatty with a straight-forward personality."
+    },
+    {
+        id: 15,
+        name: "Faye",
+        img: Faye,
+        class: "Villager",
+        age: "18",
+        description: "A childhood friend of Alm’s who lives in Ram Village. Has been dreaming of Alm ever since childhood. She joined the Deliverance to keep close to Alm."
+    },
+    {
+        id: 16,
+        name: "Fernand",
+        img: Fernand,
+        class: "Cavalier",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 17,
+        name: "Forsyth",
+        img: Forsyth,
+        class: "Soldier",
+        age: "25",
+        description: ""
+    },
+    {
+        id: 18,
+        name: "Genny",
+        img: Genny,
+        class: "Cleric",
+        age: "15",
+        description: "A cleric who lives in the Novis Priory. She’s like a little sister to Celica. She accompanies Celica on her journey towards the Temple of Mila."
+    },
+    {
+        id: 19,
+        name: "Gray",
+        img: Gray,
+        class: "Villager",
+        age: "18",
+        description: "A childhood friend of Alm’s who lives in Ram Village. Has an aloof personality. Since he was born in a merchant family he’s chatty and well informed. Along with Alm, he joined the Deliverance."
+    },
+    {
+        id: 20,
+        name: "Grieth",
+        img: Grieth,
+        class: "Dread Fighter",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 21,
+        name: "Jedah",
+        img: Jedah,
+        class: "Cantor",
+        age: "147",
+        description: ""
+    },
+    {
+        id: 22,
+        name: "Jesse",
+        img: Jesse,
+        class: "Mercenary",
+        age: "24",
+        description: ""
+    },
+    {
+        id: 23,
+        name: "Kamui",
+        img: Kamui,
+        class: "Mercenary",
+        age: "28",
+        description: "A mercenary from a distant nation. He was hired by Valbar for the purpose of revenge. Has a carefree and easy-going personality. A freedom-loving drifter who hates being tied down."
+    },
+    {
+        id: 24,
+        name: "Kliff",
+        img: Kliff,
+        class: "Villager",
+        age: "15",
+        description: "A childhood friend of Alm’s who lives in Ram Village. Loves reading books and studying history, and acts surprisingly maturely for his age. Because he was interested in seeing the world, he joined the Deliverance."
+    },
+    {
+        id: 25,
+        name: "Leon",
+        img: Leon,
+        class: "Archer",
+        age: "24",
+        description: "A pretty boy ex-soldier who follows his big brother figure Valbar. Has a frank personality and a sharp tongue, but he’s an honest man who tells no lies."
+    },
+    {
+        id: 26,
+        name: "Lukas",
+        img: Lukas,
+        class: "Soldier",
+        age: "24",
+        description: "A pretty boy ex-soldier who follows his big brother figure Valbar. Has a frank personality and a sharp tongue, but he’s an honest man who tells no lies."
+    },
+    {
+        id: 27,
+        name: "Luthier",
+        img: Luthier,
+        class: "Mage",
+        age: "22",
+        description: "A Mage who lives in a village located at the Zofian borders. He has fully devoted himself to mastering the art of sorcery and shows nary an interest to the people of society. Finds it impossible to deal with his younger sister’s selfish streaks."
+    },
+    {
+        id: 28,
+        name: "Mae",
+        img: Mae,
+        class: "Mage",
+        age: "17",
+        description: "A mage who works at the Novis Priory. Her role is Celica’s servant, but the two are also close friends. She accompanies Celica on her journey towards the Temple of Mila."
+    },
+    {
+        id: 29,
+        name: "Mathilda",
+        img: Mathilda,
+        class: "Paladin",
+        age: "28",
+        description: ""
+    },
+    {
+        id: 30,
+        name: "Mila",
+        img: Mila,
+        class: "???",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 31,
+        name: "Mycen",
+        img: Mycen,
+        class: "Gold Knight",
+        age: "65",
+        description: ""
+    },
+    {
+        id: 32,
+        name: "Nomah",
+        img: Nomah,
+        class: "Sage",
+        age: "71",
+        description: ""
+    },
+    {
+        id: 33,
+        name: "Nuibaba",
+        img: Nuibaba,
+        class: "Witch",
+        age: "124",
+        description: ""
+    },
+    {
+        id: 34,
+        name: "Palla",
+        img: Palla,
+        class: "Pegasus Knight",
+        age: "23",
+        description: "The eldest of the three Whitewing sisters from Archanea. She’s mild-mannered and down to earth, always caring about her sisters. However she also has a stubborn side and absolutely will not budge after a decision is made."
+    },
+    {
+        id: 35,
+        name: "Python",
+        img: Python,
+        class: "Archer",
+        age: "25",
+        description: "A member of the Deliverance. Born in a local town, he was encouraged by a childhood friend to join the army. He tends to view the world from a cynical angle, but he can be surprisingly serious and performs his duties with diligence."
+    },
+    {
+        id: 36,
+        name: "Randal",
+        img: Randal,
+        class: "Paladin",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 37,
+        name: "Rinea",
+        img: Rinea,
+        class: "???",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 38,
+        name: "Rudolf",
+        img: Rudolf,
+        class: "Gold Knight",
+        age: "50",
+        description: ""
+    },
+    {
+        id: 39,
+        name: "Shade",
+        img: Shade,
+        class: "Saint",
+        age: "???",
+        description: ""
+    },
+    {
+        id: 40,
+        name: "Silque",
+        img: Silque,
+        class: "Cleric",
+        age: "19",
+        description: "The eldest of the three Whitewing sisters from Archanea. She’s mild-mannered and down to earth, always caring about her sisters. However she also has a stubborn side and absolutely will not budge after a decision is made."
+    },
+    {
+        id: 41,
+        name: "Slayde",
+        img: Slayde,
+        class: "Cavalier",
+        age: "32",
+        description: ""
+    },
+    {
+        id: 42,
+        name: "Sonya",
+        img: Sonya,
+        class: "Mage",
+        age: "25",
+        description: "A female mage who guards the south desert passage leading to the headquarters of Grieth the pillager. She stands in Celica’s path, preventing her from crossing the desert. Cool and unyielding, she’s a lady who oozes womanly charm. Apparently burdened with an awful past, but hates to talk about it."
+    },
+    {
+        id: 43,
+        name: "Tatiana",
+        img: Tatiana,
+        class: "Saint",
+        age: "28",
+        description: ""
+    },
+    {
+        id: 44,
+        name: "Tobin",
+        img: Tobin,
+        class: "Villager",
+        age: "17",
+        description: "A childhood friend of Alm’s who lives in Ram Village. He’s the eldest son of a big family and fights for the Deliverance to earn money. He’s straightforward, simple and serious, but terrible at reading the mood."
+    },
+    {
+        id: 45,
+        name: "Valbar",
+        img: Valbar,
+        class: "Knight",
+        age: "31",
+        description: "An ex-soldier who seeks revenge on Barth, the pirate who murdered his family. He joined Celica to exact his vengeance. Headstrong and always trying to help people with nary a regard of the consequences."
+    },
+    {
+        id: 46,
+        name: "Yuzu",
+        img: Yuzu,
+        class: "Priestess",
+        age: "",
+        description: ""
+    },
+    {
+        id: 47,
+        name: "Zeke",
+        img: Zeke,
+        class: "Gold Knight",
+        age: "29",
+        description: "A knight of unknown origin who serves the Rigelian Empire. Has lost all recollection of his past, including his original name. After washing up at Rigel, his undeniable skills earned him the position of general. He opposes Alm not merely because he’s a Rigelian general, but because of some other reason."
+    },
+    {
+        id: "48",
+        name: "???",
+        img: Conrad,
+        class: "Paladin",
+        age: "21",
+        description: "A mysterious man who appeared before Celica during her time of need. His true face and intentions are hidden by his mask. He appears to be a capable knight, but is he a friend or foe…?"
+    }
+]
+
+export default CharacterInfo;
