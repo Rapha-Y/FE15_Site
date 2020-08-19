e61ecea3faca316071c4a53812e2856bc7fad690
Rapha-Y <raph.yuki@gmail.com>
2020-08-19 12:59:11 -0300

    tweaked GameplayExample text's field padding

--- src/components/GameplayExample.js
+++ src/components/GameplayExample.js
@@ -10,7 +10,7 @@ function GameplayExample(props) {
                     md={5} 
                     className="d-flex align-items-center justify-content-center"
                 >
-                    <Container>
+                    <Container className="py-4">
                         {props.content}
                     </Container>
                 </Col>
