fff3b09d8a24d0214aa3267d16f6feba78ba6ca0
Rapha-Y <raph.yuki@gmail.com>
2020-08-20 18:29:09 -0300

    added automatic scroll on tab change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <Body />
         <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,16 +13,40 @@ function Footer() {
                                 <h5>Official Links</h5>
                             </li>
                             <li>
-                                <a href="https://www.facebook.com/Nintendo" target="_blank">Facebook</a>
+                                <a 
+                                    href="https://www.facebook.com/Nintendo" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Facebook
+                                </a>
                             </li>
                             <li>
-                                <a href="https://www.instagram.com/nintendo/" target="_blank">Instagram</a>
+                                <a 
+                                    href="https://www.instagram.com/nintendo/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Instagram
+                                </a>
                             </li>
                             <li>
-                                <a href="https://twitter.com/NintendoAmerica" target="_blank">Twitter</a>
+                                <a 
+                                    href="https://twitter.com/NintendoAmerica" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Twitter
+                                </a>
                             </li>
                             <li>
-                                <a href="https://www.youtube.com/nintendo" target="_blank">Youtube</a>
+                                <a 
+                                    href="https://www.youtube.com/nintendo" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Youtube
+                                </a>
                             </li>
                         </ul>
                     </Col>
@@ -32,13 +56,31 @@ function Footer() {
                                 <h5>Resources</h5>
                             </li>
                             <li>
-                                <a href="https://serenesforest.net" target="_blank">Serenes Forest</a>
+                                <a 
+                                    href="https://serenesforest.net" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Serenes Forest
+                                </a>
                             </li>
                             <li>
-                                <a href="https://fireemblemwiki.org" target="_blank">Wiki</a>
+                                <a 
+                                    href="https://fireemblemwiki.org" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Wiki
+                                </a>
                             </li>
                             <li>
-                                <a href="https://fireemblem.fandom.com/wiki/Fire_Emblem_Wiki" target="_blank">Wikia</a>
+                                <a 
+                                    href="https://fireemblem.fandom.com/wiki/Fire_Emblem_Wiki" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Wikia
+                                </a>
                             </li>
                         </ul>
                     </Col>
@@ -48,10 +90,22 @@ function Footer() {
                                 <h5>Community</h5>
                             </li>
                             <li>
-                                <a href="https://aminoapps.com/c/fire-emblem-amino/home/" target="_blank">Amino</a>
+                                <a 
+                                    href="https://aminoapps.com/c/fire-emblem-amino/home/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Amino
+                                </a>
                             </li>
                             <li>
-                                <a href="https://www.reddit.com/r/fireemblem/" target="_blank">Reddit</a>
+                                <a 
+                                    href="https://www.reddit.com/r/fireemblem/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Reddit
+                                </a>
                             </li>
                         </ul>
                     </Col>
@@ -61,27 +115,47 @@ function Footer() {
                                 <h5>Other Titles</h5>
                             </li>
                             <li>
-                                <a href="https://www.nintendo.com/games/detail/fire-emblem-three-houses-switch/" target="_blank">
+                                <a 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-three-houses-switch/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
                                     Three Houses
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.nintendo.com/games/detail/fire-emblem-awakening-3ds/" target="_blank">
+                                <a 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-awakening-3ds/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
                                     Awakening
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.nintendo.com/games/detail/fire-emblem-shadow-dragon-wii-u/" target="_blank">
+                                <a 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-shadow-dragon-wii-u/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
                                     Shadow Dragon
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.nintendo.com/games/detail/fire-emblem-the-sacred-stones-wii-u/" target="_blank">
+                                <a 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-the-sacred-stones-wii-u/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
                                     The Sacred Stones
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.nintendo.com/games/detail/fire-emblem-wii-u/" target="_blank">
+                                <a 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-wii-u/" 
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
                                     The Blazing Blade
                                 </a>
                             </li>
