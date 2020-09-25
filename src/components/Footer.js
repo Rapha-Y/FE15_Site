d7174c9d1a487bac3578a7d8fd6071b6053bb7c9
Rapha-Y <raph.yuki@gmail.com>
2020-09-25 16:00:47 -0300

    added social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,77 +9,136 @@ function Footer() {
         <Jumbotron style={{margin: 0}} fluid>
             <Container>
                 <Row>
-                    <Col md={3}>
+                    <Col md={3} className="pl-4">
                         <ul className="list-unstyled">
                             <li>
                                 <h5>Official Links</h5>
                             </li>
                             <li>
-                                <FooterLink name="Facebook" href="https://www.facebook.com/Nintendo" />
+                                <FooterLink 
+                                    iconName="facebook"
+                                    name="Facebook" 
+                                    href="https://www.facebook.com/Nintendo" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Instagram" href="https://www.instagram.com/nintendo/" />
+                                <FooterLink 
+                                    iconName="instagram"
+                                    name="Instagram" 
+                                    href="https://www.instagram.com/nintendo/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Twitter" href="https://twitter.com/NintendoAmerica" />
+                                <FooterLink 
+                                    iconName="twitter"
+                                    name="Twitter" 
+                                    href="https://twitter.com/NintendoAmerica" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Youtube" href="https://www.youtube.com/nintendo" />
+                                <FooterLink 
+                                    iconName="youtube"
+                                    name="Youtube" 
+                                    href="https://www.youtube.com/nintendo" 
+                                />
                             </li>
                         </ul>
                     </Col>
-                <Col md={3}>
+                    <Col md={3} className="pl-4">
                         <ul className="list-unstyled">
                             <li>
                                 <h5>Resources</h5>
                             </li>
                             <li>
-                                <FooterLink name="Serenes Forest" href="https://serenesforest.net" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="Serenes Forest" 
+                                    href="https://serenesforest.net" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Wiki" href="https://fireemblemwiki.org" />
+                                <FooterLink
+                                    iconName={null} 
+                                    name="Wiki" 
+                                    href="https://fireemblemwiki.org" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Wikia" href="https://fireemblem.fandom.com/wiki/Fire_Emblem_Wiki" />
-
+                                <FooterLink 
+                                    iconName={null}
+                                    name="Wikia" 
+                                    href="https://fireemblem.fandom.com/wiki/Fire_Emblem_Wiki" 
+                                />
                             </li>
                         </ul>
                     </Col>
-                    <Col md={3}>
+                    <Col md={3} className="pl-4">
                         <ul className="list-unstyled">
                             <li>
                                 <h5>Community</h5>
                             </li>
                             <li>
-                                <FooterLink name="Amino" href="https://aminoapps.com/c/fire-emblem-amino/home/" />
+                                <FooterLink
+                                    iconName={null} 
+                                    name="Amino" 
+                                    href="https://aminoapps.com/c/fire-emblem-amino/home/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Reddit" href="https://www.reddit.com/r/fireemblem/" />
+                                <FooterLink
+                                    iconName={null} 
+                                    name="Reddit" 
+                                    href="https://www.reddit.com/r/fireemblem/" 
+                                />
                             </li>
                         </ul>
                     </Col>
-                    <Col md={3}>
+                    <Col md={3} className="pl-4">
                         <ul className="list-unstyled">
                             <li>
                                 <h5>Previous Titles</h5>
                             </li>
                             <li>
-                                <FooterLink name="Fates: Conquest" href="https://www.nintendo.com/games/detail/fire-emblem-fates-conquest-3ds/" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="Fates: Conquest" 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-fates-conquest-3ds/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Fates: Birthright" href="https://www.nintendo.com/games/detail/fire-emblem-fates-birthright-3ds/" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="Fates: Birthright" 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-fates-birthright-3ds/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Awakening" href="https://www.nintendo.com/games/detail/fire-emblem-awakening-3ds/" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="Awakening" 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-awakening-3ds/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="Shadow Dragon" href="https://www.nintendo.com/games/detail/fire-emblem-shadow-dragon-wii-u/" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="Shadow Dragon" 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-shadow-dragon-wii-u/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="The Sacred Stones" href="https://www.nintendo.com/games/detail/fire-emblem-the-sacred-stones-wii-u/" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="The Sacred Stones" 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-the-sacred-stones-wii-u/" 
+                                />
                             </li>
                             <li>
-                                <FooterLink name="The Blazing Blade" href="https://www.nintendo.com/games/detail/fire-emblem-wii-u/" />
+                                <FooterLink 
+                                    iconName={null}
+                                    name="The Blazing Blade" 
+                                    href="https://www.nintendo.com/games/detail/fire-emblem-wii-u/" 
+                                />
                             </li>
                         </ul>
                     </Col>
--- src/components/FooterLink.js
+++ src/components/FooterLink.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { MDBIcon } from 'mdbreact';
 
 function FooterLink(props) {
-    return (
-        <a 
-            href={props.href}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            {props.name}
-        </a>
-    );
+    if (props.iconName === null) {
+        return (
+            <div>
+                <a 
+                    href={props.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {props.name}
+                </a>
+            </div>
+        );
+    } else {
+        const iconProps = props.iconName + " pr-2";
+        return (
+            <div>
+                <a 
+                    href={props.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <MDBIcon fab icon={iconProps} />
+                    {props.name}
+                </a>
+            </div>
+        );
+    }
 }
 
 export default FooterLink;
